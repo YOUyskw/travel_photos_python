39f4c4cdec245489bf4ce17586f4b051e01dfc05
yutorse <yutohorse.coc@gmail.com>
2023-03-17 18:46:15 +0900

    不要なファイル削除

--- lib/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    for i in range(len(groups)):
-        for photo in groups[i]:
-            url = photo["downloadUrl"]
-            photo_id = photo_id_dict[f"{url}"]
-            db.collection("group").document(f"{group_id}").collection("grouping_photo").document(f"{i}").collection(f"{i}").document(f"{photo_id}").set(photo)
